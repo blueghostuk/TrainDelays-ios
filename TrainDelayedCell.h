b719ebcd2d706dd662b8de0f5838d8182479d3b6
mpritchard <blueghostuk@gmail.com>
2014-03-06 23:18:35 +0000

    add new UITableViewCell implementation
    contains crd code

---
+++ TrainDelayedCell.h
@@ -0,0 +1,15 @@
+//
+//  TrainDelayedCell.h
+//  TrainDelays
+//
+//  Created by Michael Pritchard on 06/03/2014.
+//  Copyright (c) 2014 Michael Pritchard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrainDelayedCell : UITableViewCell
+
+@property (nonatomic) NSString *crsCode;
+
+@end
---
+++ TrainDelayedCell.m
@@ -0,0 +1,29 @@
+//
+//  TrainDelayedCell.m
+//  TrainDelays
+//
+//  Created by Michael Pritchard on 06/03/2014.
+//  Copyright (c) 2014 Michael Pritchard. All rights reserved.
+//
+
+#import "TrainDelayedCell.h"
+
+@implementation TrainDelayedCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
